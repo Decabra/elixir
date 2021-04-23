b753b1d7018b2a7cf799d3ceb1c784ce5d4aeba0
bovad <hayub.bese18seecs@seecs.edu.pk>
2021-04-23 10:41:33 +0500

    added background image and width set

--- src/app/frontend/Login/Login.js
+++ src/app/frontend/Login/Login.js
@@ -1,44 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Login.module.scss';
-import BgImg from '../../../assets/img/petals.svg'
+import BgImg from '../../../assets/img/petals.png'
 
 const Login = () => (
-  <section className="section">
-    <div className="container">
-      <div className="columns is-centered">
-        <div className="column box is-mobile is-one-third p-5">
-          
-          <p className="is-size-3 is-size-4-mobile has-text-centered mb-5 mt-2"><b>Sign in</b></p>
-          
-          <form>
-          
-            <div className="field">
-              <label className="label">Email</label>
-              <div className="control">
-                <input className="input" type="email" placeholder="e.g. alex@example.com" />
-              </div>
+  <div className={styles.bgImg}>
+
+    <section className={"section mt-6"}>
+      <div className="container">
+        <div className="columns is-centered">
+          <div className={"column box is-mobile is-5-desktop is-8-tablet p-5"}>
+            <div className={styles.shadow}>
+
+              <p className="is-size-3 is-size-4-mobile has-text-centered mb-5 mt-2"><b className={styles.letterSpacing}>Sign in</b></p>
+              
+              <form>
+              
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input className="input" type="email" placeholder="e.g. alex@example.com" />
+                  </div>
+                </div>
+              
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control">
+                    <input className="input" type="password" placeholder="********" />
+                  </div>
+                </div>
+              
+                <a className="has-text-link">Forgot password?</a>
+
+                <button className="button mt-4 is-link is-fullwidth is-size-5">Sign in</button>
+
+                <p className="mt-4">Don't have an account?  <a className="has-text-link ml-2">Sign up</a> </p>
+
+              </form>
+
             </div>
-          
-            <div className="field">
-              <label className="label">Password</label>
-              <div className="control">
-                <input className="input" type="password" placeholder="********" />
-              </div>
-            </div>
-          
-            <a className="has-text-link">Forgot password?</a>
 
-            <button className="button mt-4 is-link is-fullwidth is-size-5">Sign in</button>
-
-            <p className="mt-4">Don't have an account?  <a className="has-text-link ml-2">Sign up</a> </p>
-
-          </form>
-
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+  </div>
+  
 
 );
 
