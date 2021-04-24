c92b406d5fb6f0aff96317fa9b17ed9180c580d0
bovad <hayub.bese18seecs@seecs.edu.pk>
2021-04-24 14:46:18 +0500

    Letter Spacing added

--- src/app/frontend/Heading/Heading.js
+++ src/app/frontend/Heading/Heading.js
@@ -4,7 +4,7 @@ import styles from './Heading.module.scss';
 
 const Heading = (props) => (
     <div className={styles.box}>
-        <p className="is-size-3 has-text-centered py-5 has-text-weight-semibold">{props.text}</p>
+        <p className="is-size-3 has-text-centered py-5 has-text-weight-bold">{props.text}</p>
     </div>
 )
 
