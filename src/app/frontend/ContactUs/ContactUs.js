48575840d6dab59ce5348ff6b973ba40eed2f521
bovad <hayub.bese18seecs@seecs.edu.pk>
2021-04-24 16:53:53 +0500

    created contact us page

--- src/app/frontend/ContactUs/ContactUs.js
+++ src/app/frontend/ContactUs/ContactUs.js
@@ -9,19 +9,21 @@ const ContactUs = () => (
   <div className={styles.ContactUs} data-testid="ContactUs">
       <Header/>
       <Heading text="Elixir Support 24/7" />
+      <div className={styles.centerForm}>
+        <div className="container">
+          <p className="is-size-4 mt-6 mb-4">How can we help?</p>
+          <form>
+            <input className="input my-2" type="text" placeholder="Your name e.g., Hamid Ayub" />
+            <input className="input my-2" type="email" placeholder="Your email e.g., abc@example.com" />
+            <textarea className="textarea my-2" placeholder="What you want to say?" rows="9"></textarea>
+            <button className="button is-link is-pulled-right my-2 py-4 px-6">Submit</button> <br/>
+          </form>
+        </div>
+      </div>
       <Footer />
   </div>
 );
 
-// <div className="container">
-//         <p className="is-size-4 mt-6 mb-4">How can we help?</p>
-//         <form>
-//           <input className="input my-2" type="email" placeholder="e.g. alex@example.com" />
-//           <input className="input my-2" type="password" placeholder="********" />
-//           <textarea className="textarea my-2" placeholder="10 lines of textarea" rows="10"></textarea>
-//           <button className="button is-link is-pulled-right my-2 py-4 px-6">Submit</button> <br/>
-//         </form>
-//       </div>
 
 ContactUs.propTypes = {};
 
