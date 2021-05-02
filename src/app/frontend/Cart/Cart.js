fb04b5afe242374cdb8b8c0f58f83e61a439fe86
itsumernustian <umerfarooq@gmail.com>
2021-05-02 12:59:40 +0500

    Bhaio Git chalana bohat Muskhil hai

--- src/app/frontend/Cart/Cart.js
+++ src/app/frontend/Cart/Cart.js
@@ -1,59 +1,126 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Cart.module.scss';
+import styles from './Cart.scss';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import img1 from '../../../assets/img/right.jpg';
+import {Link} from "react-router-dom";
 
-const Cart = () => (
-  <div className={styles.Cart} data-testid="Cart">
-      <Header/>
-          <form>
-              <h3 className="container">Shopping Cart</h3>
-              <ul className="nav nav-pills  nav-fill nav-cart">
-                  <li className="nav-item">
-                      <button className="btn btn-lg btn-secondary " type="submit">Continue Shopping</button>
-                  </li>
-                  <li className="nav-item">
-                      <p className="text-center nav-item-textcounter">You have 1 item in your cart</p>
-                  </li>
-                  <li className="nav-item">
-                      <button type="button" className="btn btn-lg btn-dark">Empty Cart</button>
-                  </li>
 
-              </ul>
-              <div className="shadow p-3 mb-5 bg-body rounded item-body" >
-                  <div className="ordered-item-arrangement" >
-                      {/*img1 is not defined anywhere*/}
-                      {/*<img src={img1} className="rounded item-ordered-image " alt="..."/>*/}
-                      <article className="template-name"><a href="#" className="nav-link active text-lg-start fs-1">Template Name XYZ </a>
-                          <p className=" nav-link text-md-start">
-                              Category
-                          </p></article>
-                      <article>
-                          <button className="navbar-brand btn btn-danger item-delete" >
-                              <span className="material-icons">delete</span>Delete</button>
-                          <h4 className="item-price">$15</h4>
-                      </article>
-                  </div>
-                  <hr/>
-                  <div>
-                      <ul className="nav nav-pills navbar-light  nav-fill checkout-area" >
-                          <li className="nav-item">
-                              <button className="btn btn-md btn-secondary" type="submit">Continue Shopping</button>
-                          </li>
-                          <li className="nav-item">
-                              <h4 className="text-center text-wrap total-font-family">Total US $15</h4>
-                              <button type="button" className="btn btn-md btn-success">Checkout</button>
-                          </li>
-
-                      </ul>
-                  </div>
-              </div>
-          </form>
-
-      <Footer/>
+const Cart = () =>{
+    let checkoutLink = [
+        {name:"Checkout", path:'/checkout'},
+    ];
+  return (
+  <div  className='bg-white' data-testid="Cart">
+    <Header/>
+    <nav className={"navbar navbar-light cart-nav"}>
+      <div className="container-fluid cart-nav-head">
+        <span className="navbar-brand mb-0 h1 text-light cart-nav-text">Cart</span>
       </div>
-);
+    </nav>
+    <section className='bg-white'>
+        <div className="container pb-2 pt-4">
+            <div className="wrapper wrapper-content ">
+                <div className="row">
+                    <div className="col-8 shadow rounded bg-white mb-3 pb-3">
+                        <div className="d-flex px-4 pt-4">
+                            <div className="col-md-10 h5">
+                                Items in your cart
+                            </div>
+                            <div className="col-md-2 h6">
+                                (<strong>5</strong>) items
+                            </div>
+                        </div>
+                        <hr className='break-line'style={{height:'0.6px'}}/>
+                        <div className="d-flex px-3">
+                            <div className="col-md-3">
+                                <img className="rounded-circle" src={img1} width="100" height="100"/>
+                            </div>
+                            <div className="col-md-6">
+                                <a className="card-link h5"  href="#">Desktop publishing software</a>
+                                <h6 className='h6'>Description list</h6>
+                                <p className='h6'>A description list is perfect for defining terms.</p>
+                            </div>
+                            <div className="col-md-3 text-center">
+                                <h4 className="">$50</h4>
+                                <div className="btn-group gap-2 py-3">
+                                    <a href="#" className="text-muted"><span className="material-icons">visibility</span></a>
+                                    <a href="#" className="text-muted"><span className="material-icons text-danger">highlight_off</span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="d-flex px-3 pt-5">
+                            <div className="col-md-3">
+                                <img className="rounded-circle" src={img1} width="100" height="100"/>
+                            </div>
+                            <div className="col-md-6">
+                                <a className="card-link h5"  href="#">Desktop publishing software</a>
+                                <h6 className='h6'>Description list</h6>
+                                <p className='h6'>A description list is perfect for defining terms.</p>
+                            </div>
+                            <div className="col-md-3 text-center">
+                                <h4 className="">$50</h4>
+                                <div className="btn-group gap-2 py-3">
+                                    <a href="#" className="text-muted"><span className="material-icons">visibility</span></a>
+                                    <a href="#" className="text-muted"><span className="material-icons  text-danger">highlight_off</span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <hr className='break-line' style={{height:'0.6px'}}/>
+                        <div className="px-3 pt-2">
+                            <nav className="navbar navbar-light">
+                            <div className="container-fluid">
+                                <button className="btn btn-white  d-flex"><span className="material-icons">arrow_left
+                                </span> Continue shopping</button>
+                                 {
+                                    checkoutLink.map(({name ,path}) => (
+                                        <Link className="rounded btn btn-outline-primary btn-md d-flex" to={path}>
+                                            <span className="material-icons">shopping_cart</span> Checkout
+                                    </Link>
+                                    ))
+                                 }
+                            </div>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="card border-0 rounded shadow bg-white">
+                            <div className="card-body bg-white">
+                                <h5 className="card-title text-center">Order Summary</h5>
+                                <hr className='break-line'/>
+                                <div className="row p-2 text-center">
+                                    <p className="card-text col-md-6 h4">Total</p>
+                                    <p className="card-text col-md-6 h4">$1500</p>
+                                </div>
+                                <hr className='break-line' style={{height:'0.6px'}}/>
+                                <p className="text-md-center h6">
+                                    *For United States, France and Germany applicable sales tax will be applied
+                                </p>
+                                <div className="text-center pt-4">
+                                    <div className="btn-group gap-2">
+                                       {
+                                            checkoutLink.map(({name ,path}) => (
+                                                <Link className="rounded btn btn-outline-primary btn-md d-flex" to={path}>
+                                                    <span className="material-icons">shopping_cart</span> Checkout
+                                            </Link>
+                                            ))
+                                        }
+                                        <a href="#" className="rounded btn btn-outline-danger btn-md"> Empty Cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+    <Footer/>
+    </div>
+);}
 
 Cart.propTypes = {};
 
---
+++ src/app/frontend/Checkout/Checkout.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Checkout.scss';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+const Checkout = () => (
+  <div className='bg-white' data-testid="Cart">
+      <Header/>
+         <nav className="navbar navbar-light checkout-nav">
+		    <div className="container-fluid checkout-nav-head"  >
+		      <span className="navbar-brand mb-0 h1 text-light checkout-nav-text">Secure Checkout</span>
+		    </div>
+		 </nav>
+         <div className="container pb-5 mt-3">
+			<div className="wrapper wrapper-content animated fadeInRight">
+			    <div className="row">
+			        <div className="col-md-8 pb-5" style={{marginTop: "19px"}}>
+			                <form className="row g-3 bg-white shadow p-5">
+			                <h2 className="h2">Billings Details</h2>
+			                <div className="col-md-6">
+			                  <label for="inputFirstName" className="form-label">First Name</label>
+			                  <input type="text" className="form-control" style={{outline: "none"}} id="inputFirstName"/>
+			                </div>
+			                <div className="col-md-6">
+			                  <label for="inputLastName" className="form-label">Last Name</label>
+			                  <input type="password" className="form-control" id="inputLastName"/>
+			                </div>
+			                <div className="col-12">
+			                  <label for="inputCountry" className="form-label">Country</label>
+			                  <input type="text" className="form-control" id="inputCountry" placeholder="Pakistan , United Kingdom etc..."/>
+			                </div>
+			                <div className="row m-l-1 m p-2">
+			                    <div className="col-md-6">
+			                      <label for="inputCity" className="form-label">City</label>
+			                      <input type="text" className="form-control" id="inputCity" placeholder="Faisalabad, Multan etc..."/>
+			                    </div>
+			                    <div className="col-md-4 p-l-4">
+			                      <label for="inputZip" className="form-label">Zip</label>
+			                      <input type="text" className="form-control" id="inputZip" placeholder="zip/postal code"/>
+			                    </div>
+			                </div>
+			                <div className="col-12">
+			                  <label for="inputAddress" className="form-label">Address</label>
+			                  <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+			                </div>
+			                <h2 className="m-y-4">Select Payment Method</h2>
+			                <div className="col-12">
+			                  <label for="inputCardNumber" className="form-label">Card Number</label>
+			                  <input type="text" className="form-control" id="inputCardNumber" placeholder="card number"/>
+			                </div>
+			                <div className="row p-2">
+			                  <div className="col-md-6">
+			                    <label for="inputExpiryDate" className="form-label">Expiry Date</label>
+			                    <input type="date" className="form-control" id="inputExpiryDate"/>
+			                  </div>
+			                  <div className="col-md-6">
+			                    <label for="inputCVV" className="form-label">CVV</label>
+			                    <input type="text" className="form-control" id="inputCVV" placeholder="CVV"/>
+			                  </div>
+			                </div>
+			                <div className="d-grid d-md-flex justify-content-md-end">
+			                  <button className="btn btn-lg btn-outline-success" type="button">Make Payment</button>
+			                </div>
+			              </form>
+
+			        </div>
+			        <div className="col-md-4">
+			            <div className="card border-0">
+			              <div className="card-body">
+			                <h5 className="card-title text-center">Order Summary</h5>
+			                <div className="row">
+			                  <p className="card-text col-md-9">Shopping Website</p>
+			                  <p className="card-text col-md-3">$50</p>
+			                </div>
+			                <div className="row">
+			                  <p className="card-text col-md-9">Shopping Website</p>
+			                  <p className="card-text col-md-3">$50</p>
+			                </div>
+			                <hr style={{height:'1px',backgroundColor: "#242121"}}/>
+			                <div className="row p-2 text-center">
+			                  <p className="card-text col-md-6 h4">Total</p>
+			                  <p className="card-text col-md-6 h4">$1500</p>
+			                </div>
+			              </div>
+			            </div>
+			        </div>
+			    </div>
+			</div>
+			</div> 
+
+      <Footer/>
+      </div>
+);
+
+Checkout.propTypes = {};
+
+Checkout.defaultProps = {};
+
+export default Checkout;
