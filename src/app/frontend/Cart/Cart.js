74732794cad17148866158628a9423544bc532fb
itsumernustian <umerfarooq@gmail.com>
2021-05-06 09:39:21 +0500

    Final Version of Cart & checkout so in previos pull request you have to merge the index.html file only

--- src/app/frontend/Cart/Cart.js
+++ src/app/frontend/Cart/Cart.js
@@ -14,29 +14,29 @@ const Cart = () =>{
   return (
   <div  className='bg-white' data-testid="Cart">
     <Header/>
-    <nav className="navbar navbar-light" className={styles.cartNav}>
-      <div className="container-fluid " className={styles.cartNavHead}>
-        <span className="navbar-brand mb-0 h1 text-light " className={styles.cartNavText}>Cart</span>
+    <nav className={`navbar navbar-light ${styles.cartNav}`}>
+      <div className={`container-fluid text-center ${styles.cartNavHead}`}>
+        <span className={`navbar-brand mb-5 h1 text-light ${styles.cartNavText}`}>Cart</span>
       </div>
     </nav>
-    <section className="row " style={{margin:'20px'}}>
-    <section  className="col-md-8 shadow rounded m-auto ml-5  mr-5 mt-2 mb-5 pb-5 pt-3 container"  >
+    <section className="row" style={{margin:'20px'}}>
+    <section  className={`col-md-8 shadow rounded m-auto ml-5  mr-5 mt-2 mb-5 pb-5 pt-3 container ${styles.car}`}  >
         <section>
             <button className="btn btn-lg btn-outline-danger mx-4 mt-3">Empty Cart</button>
         </section>
         <section className="col-md-12 m-auto mx-4 pb-5 ">
             <div className="d-flex px-3 pt-5">
-                <div className="col-md-3" >
-                    <img className="rounded-circle" src={img1} width="150px" height="120px;"/>
+                <div className="col-md-3" style={{width:'25%'}}>
+                    <img className="rounded-circle" src={img1} width="120px" height="120px;"/>
                 </div>
-                <div className="col-md-6 pt-1" style={{width:'48%'}} className={styles.templateName}>
+                <div className={`col-md-6 pt-1 ${styles.templateName}`} style={{width:'48%'}}>
                     <a className="card-link h2 text-dark" style={{textDecoration:'none'}} href="#">Zapmata</a>
                     <p className="h6 text-muted text-wrap ">Online Engineering Design <br/> Tools</p>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
                     <span>(523 ratings)</span>
                 </div>
                 <div className="col-md-3 text-center pt-1">
@@ -48,17 +48,17 @@ const Cart = () =>{
                 </div>
             </div>
             <div className="d-flex px-3 pt-5">
-                <div className="col-md-3">
-                    <img className="rounded-circle" src={img1} width="150px" height="120px;"/>
+                <div className="col-md-3" style={{width:'25%'}}>
+                    <img className="rounded-circle" src={img1} width="120px" height="120px;"/>
                 </div>
-                <div className="col-md-6 pt-1 " style={{width:'48%'}} className={styles.templateName}>
+                <div className={`col-md-6 pt-1 ${styles.templateName} `} style={{width:'48%'}}>
                     <a className="card-link h2 text-dark" style={{textDecoration:'none'}} href="#">Zapmata</a>
                     <p className="h6 text-muted text-wrap ">Online Engineering Design <br/> Tools</p>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
-                    <span className="fa fa-star checked"></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
+                    <span className={`fa fa-star checked ${styles.star} `}></span>
                     <span>(523 ratings)</span>
                 </div>
                 <div className="col-md-3 text-center pt-1">
@@ -71,7 +71,7 @@ const Cart = () =>{
             </div>
         </section>
     </section>
-    <section  className={styles.card1} style={{marginRight: '10px'}}>
+    <section  className={` col-md-4 ${styles.card1}`} style={{marginRight: '25px'}}>
         <div className="rounded border-1 bg-white shadow pb-5 pt-3 text-center">
             <h3 className="h2 text-center pt-5 pb-5">Your Cart Total</h3>
             <h3 className="h2 text-center fw-bold pt-5 pb-5">$170</h3>
